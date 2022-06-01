de3926dd7e0412661ca1490923d8922b461dd91d
Clayton Spinelli <70386763+Cspinelli2000@users.noreply.github.com>
2022-06-01 03:06:02 -0400

    Add files via upload

--- ContactManager/addContact.php
+++ ContactManager/addContact.php
@@ -3,21 +3,23 @@
 	$inData = getRequestInfo();
 	
 	$userId = $inData["userId"];
-	$contactId = $inData["contactId"];
-	$status = $inData["status"];
+	$user = $inData["username"];
+	$first = $inData["first_name"];
+	$last = $inData["last_name"];
+	$num = $inData["phone_num"];
+	$email = $inData["email"];
+	$addr = $inData["address"];
 
 	require('DB_connections.php');
 	session_start();
 	
-	$sql = "INSERT INFO contact_list (user_id, contact_id, status) 
-	VALUES (:userId, :contactId, :status)";
+	$sql = "INSERT INFO contact_list (user_id, username, first_name, last_name, phone_num, email, address) 
+	VALUES (:userId, :user, :first, :last, :num, :email, :addr)";
 	
 	if ($con->query($sql) === TRUE) {
 		echo "New record created successfully";
 	} 
-	else {
+	(else {
 		echo "Error: " . $sql . "<br>" . $con->error;
 	}
 ?>
-
-basic html + xampp to host -> testing
--- ContactManager/deleteContact.php
+++ ContactManager/deleteContact.php
@@ -3,14 +3,19 @@
 	$inData = getRequestInfo();
 	
 	$userId = $inData["userId"];
-	$contactId = $inData["contactId"];
-	$status = $inData["status"];
+	$user = $inData["username"];
+	$first = $inData["first_name"];
+	$last = $inData["last_name"];
+	$num = $inData["phone_num"];
+	$email = $inData["email"];
+	$addr = $inData["address"];
 	
 	require('DB_connections.php');
 	session_start();
 	
 	$sql = "DELETE FROM contact_list 
-	WHERE user_id = :userId AND contact_id = :contactId";
+	WHERE user_id = :userId AND username = :user AND first_name = :first AND last_name = :last
+	AND phone_num = :num AND email = :email AND address = :addr";
 
 	if ($con->query($sql) === TRUE) {
 		echo "Record deleted successfully";
