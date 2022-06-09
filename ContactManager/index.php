1fe7f0a2b7329e4929e5ebe720509843f74eab5e
Gian-Alvarez <106183872+Gian-Alvarez@users.noreply.github.com>
2022-06-09 15:04:17 -0400

    Add files via upload

--- ContactManager/index.php
+++ ContactManager/index.php
@@ -1,123 +1,345 @@
-<?php
-
-require('DB_connections.php');
-session_start();
-
-$query = "SELECT * FROM `contact_list` WHERE `user_id` = '1' LIMIT 10";
-
-$result = mysqli_query($con, $query);
-if (mysqli_num_rows($result) > 0) {?>
-
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css" />
-    <title>Login</title>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Emoji:wght@700&display=swap" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link href="https://fonts.googleapis.com/css2?family=Noto+Emoji:wght@500;700&family=Noto+Sans+Symbols+2&family=Pacifico&display=swap" rel="stylesheet">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  font-family: "Lato", sans-serif;
+    background: linear-gradient(#f2eade, #e7d4b3);
+}
+
+.mainNav{
+    
+}
+.intro,
+.intro a{
+  color:#fff;
+font-family: 'Noto Emoji', sans-serif;
+font-family: 'Noto Sans Symbols 2', sans-serif;
+}
+/* customizable snowflake styling */
+.snowflake {
+  color: #fff;
+  font-size: 2em;
+font-family: 'Noto Emoji', sans-serif;
+font-family: 'Noto Sans Symbols 2', sans-serif;
+
+}
+@-webkit-keyframes snowflakes-fall{0%{top:-10%}100%{top:100%}}@-webkit-keyframes snowflakes-shake{0%{-webkit-transform:translateX(0px);transform:translateX(0px)}50%{-webkit-transform:translateX(80px);transform:translateX(80px)}100%{-webkit-transform:translateX(0px);transform:translateX(0px)}}@keyframes snowflakes-fall{0%{top:-10%}100%{top:100%}}@keyframes snowflakes-shake{0%{transform:translateX(0px)}50%{transform:translateX(80px)}100%{transform:translateX(0px)}}.snowflake{position:fixed;top:-10%;z-index:9999;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;cursor:default;-webkit-animation-name:snowflakes-fall,snowflakes-shake;-webkit-animation-duration:10s,3s;-webkit-animation-timing-function:linear,ease-in-out;-webkit-animation-iteration-count:infinite,infinite;-webkit-animation-play-state:running,running;animation-name:snowflakes-fall,snowflakes-shake;animation-duration:10s,3s;animation-timing-function:linear,ease-in-out;animation-iteration-count:infinite,infinite;animation-play-state:running,running}.snowflake:nth-of-type(0){left:1%;-webkit-animation-delay:0s,0s;animation-delay:0s,0s}.snowflake:nth-of-type(1){left:10%;-webkit-animation-delay:1s,1s;animation-delay:1s,1s}.snowflake:nth-of-type(2){left:20%;-webkit-animation-delay:6s,.5s;animation-delay:6s,.5s}.snowflake:nth-of-type(3){left:30%;-webkit-animation-delay:4s,2s;animation-delay:4s,2s}.snowflake:nth-of-type(4){left:40%;-webkit-animation-delay:2s,2s;animation-delay:2s,2s}.snowflake:nth-of-type(5){left:50%;-webkit-animation-delay:8s,3s;animation-delay:8s,3s}.snowflake:nth-of-type(6){left:60%;-webkit-animation-delay:6s,2s;animation-delay:6s,2s}.snowflake:nth-of-type(7){left:70%;-webkit-animation-delay:2.5s,1s;animation-delay:2.5s,1s}.snowflake:nth-of-type(8){left:80%;-webkit-animation-delay:1s,0s;animation-delay:1s,0s}.snowflake:nth-of-type(9){left:90%;-webkit-animation-delay:3s,1.5s;animation-delay:3s,1.5s}
+/* Demo Purpose Only*/
+.demo {
+  font-family: 'Raleway', sans-serif;
+  color:#fff;
+    display: block;
+    margin: 0 auto;
+    padding: 15px 0;
+    text-align: center;
+}
+.demo a{
+  font-family: 'Raleway', sans-serif;
+color: #000;    
+}
+.sideLog {
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  z-index: 5;
+  top: 0;
+  left: 0;
+  background: linear-gradient(#f2eade, #e7d4b3);
+  background-size: 100%;
+  background-repeat: no-repeat;
+  background-position: bottom;
+  overflow-x: hidden;
+  transition: 0.5s;
+  padding-top: 60px;
+}
+
+.sideLog a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
+}
+
+.sideLog a:hover {
+  color: #f1f1f1;
+}
+
+.sideLog .closebtn {
+  position: absolute;
+  top: 0;
+  right: 25px;
+  font-size: 36px;
+  margin-left: 50px;
+}
+#log {
+  height: 100%;
+  text-align: center;
+  background-color: transparent;
+  margin-top: 20px;
+}
+#sign {
+  display: none;
+  height: 100%;
+  text-align: center;
+  background-color: transparent;
+  margin-top: 20px;
+}
+
+@media screen and (max-height: 450px) {
+  .sidenLog {padding-top: 15px;}
+  .sideLog a {font-size: 18px;}
+}
+</style>
 </head>
-
 <body>
-    <h1>User: <?php echo $userName ?></h1>
-    <h2>Contacts:<br></h2>
-    <div class="searchBar">
-        <input type="text" class="form-control" id="live-search" autocomplete="off" placeholder="Search...">
-
-    </div>
-    <div class="displayResult">
-    <table class="resultTable">
-            <thead>
-                <tr>
-                    <th>Username</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Email</th>
-                    <th>Address</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                <?php
-                    while($row = mysqli_fetch_assoc($result)) {
-
-                        $username = $row['username'];
-                        $password = $row['password'];
-                        $firstName = $row['first_name'];
-                        $lastName = $row['last_name'];
-                        $email = $row['email'];
-                        $address = $row['address'];
-
-                    ?>
-
-                    <tr>
-                        <td><?php echo $username;?></td>
-                        <td><?php echo $firstName;?></td>
-                        <td><?php echo $lastName;?></td>
-                        <td><?php echo $email;?></td>
-                        <td><?php echo $address;?></td>
-                    </tr>
-                <?php } ?>
-            </tbody>
-
-        </table>
-                    <?php
-    } else {
-        echo "<h6 class = 'text-danger'>No data found</h6>";
-    }
-?>
-    </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
 
-    <script>
-        $(document).ready(function(){
-            $("#live-search").keyup(function(){
-                var input = $(this).val();
-                    // alert(input);
-                    if(input != " "){
-                        $.ajax({
-                            url: "live-search.php",
-                            method: "POST",
-                            data:{input:input},
 
-                            success:function(data){
-                                $(".displayResult").html(data);
-                            }
-                        });
-                    }
-                    else {
-                        // $(".displayResult").css("display", "none");
-                    }
-            })
-        })
-    </script>
+<div class = "mainNav">
+<div id="mySidenav" class="sideLog"><center>
+<div class="snowflakes" aria-hidden="true">
+   <div class="snowflake" style = "font-family: 'Noto Emoji', sans-serif;">
+<img style = "border-radius: 0px; border: 0px; box-shadow: 0; background: transparent;box-shadow: 0px 0px 0px 0px rgba(239, 134, 55,1); width: 15%" src="https://imgur.com/qmOn1dT.png">
+  </div>
+  <div class="snowflake">
+<img style = "border-radius: 0px; border: 0px; box-shadow: 0; background: transparent;box-shadow: 0px 0px 0px 0px rgba(239, 134, 55,1); width: 15%" src="https://imgur.com/l6JPg4g.png">
+  </div>
+  <div class="snowflake" style = "font-family: 'Noto Emoji', sans-serif;">
+<img style = "border-radius: 0px; border: 0px; box-shadow: 0; background: transparent;box-shadow: 0px 0px 0px 0px rgba(239, 134, 55,1); width: 15%" src="https://imgur.com/WfjYz9e.png">
+  </div>
+  <div class="snowflake">
+<img style = "border-radius: 0px; border: 0px; box-shadow: 0; background: transparent;box-shadow: 0px 0px 0px 0px rgba(239, 134, 55,1); width: 15%" src="https://imgur.com/PudNcRw.png">
+  </div>
+  <div class="snowflake">
+<img style = "border-radius: 0px; border: 0px; box-shadow: 0; background: transparent;box-shadow: 0px 0px 0px 0px rgba(239, 134, 55,1); width: 15%" src="https://imgur.com/l6JPg4g.png">
+  </div>
+  <div class="snowflake">
+<img style = "border-radius: 0px; border: 0px; box-shadow: 0; background: transparent;box-shadow: 0px 0px 0px 0px rgba(239, 134, 55,1); width: 15%" src="https://imgur.com/l6JPg4g.png">
+  </div>
+  <div class="snowflake">
+<img style = "border-radius: 0px; border: 0px; box-shadow: 0; background: transparent;box-shadow: 0px 0px 0px 0px rgba(239, 134, 55,1); width: 15%" src="https://imgur.com/PudNcRw.png">
+  </div>
+  <div class="snowflake">
+<img style = "border-radius: 0px; border: 0px; box-shadow: 0; background: transparent;box-shadow: 0px 0px 0px 0px rgba(239, 134, 55,1); width: 15%" src="https://imgur.com/PudNcRw.png">
+  </div>
+  <div class="snowflake">
+<img style = "border-radius: 0px; border: 0px; box-shadow: 0; background: transparent;box-shadow: 0px 0px 0px 0px rgba(239, 134, 55,1); width: 15%" src="https://imgur.com/qmOn1dT.png">
+  </div>
+  <div class="snowflake">
 
+</div>
+</div>
+  <h style = "font-family: 'Pacifico', cursive; text-align: center; font-size: 4vw; width: 50%"> &nbspTreehouse</h>
+  <br>
+  <label style = "font-size: 10px">•contact management application•</label>
 
-</body>
+<div id="log">
+<form onsubmit = "return false", style = "top:50%;">
+  <label for="username">Username</label><br>
+  <input type="text" id="username" name="username" palceholder="" style = "border: 0px;"><br>
+  <label for="password">Password</label><br>
+  <input type="password" id="password" name="password" placeholder="" style = "border: 0px;">
+  <br>
+  <br>
+  <input type="submit" onclick = "doLogin()" style ="background: linear-gradient(to right,#6fc80f,#86d72f); color: white; border: 0px; height: 20px; width: 50px;"  value="log-in"><br>
+  <span id="loginResult"></span>
+  <br>
+</form>
+<label style = "font-size: 12px">need an account?</label><button style = "font-size: 12px; background-color: transparent; border: 0px; color:#86d72f;  " onclick="signFunction()">sign-up!</button>
+</div>
 
-</html>
+<div id="sign">
+<form>
+  <label for="username">Username</label><br>
+  <input type="text" id="username" name="username" placeholder="" style = "border: 0px;"><br>
+  <label for="password">Password</label><br>
+  <input type="password" id="password" name="password" placeholder="" style = "border: 0px;"><br>
+  <label for="first_name">First Name</label><br>
+  <input type="text" id="first_name" name="first_name" placeholder="" style = "border: 0px;"><br>
+  <label for="last_name">Last Name</label><br>
+  <input type="text" id="last_name" name="last_name" placeholder="" style = "border: 0px;"><br>
+  <label for="email">Email</label><br>
+  <input type="text" id="email" name="email" placeholder="" style = "border: 0px;"><br>
+  <label for="phone">Phone</label><br>
+  <input type="text" id="phone_num" name="phone_num" placeholder="" style = "border: 0px;"> <br>
+  <label for="address">Address</label><br>
+  <input type="text" id="address" name="address" placeholder="" style = "border: 0px;">
+  <br>
+  <br>
+  <input type="submit" onclick = "doRegister()" style ="background: linear-gradient(to right,#de1a18,#f8312f); color: white; border: 0px; height: 20px; width: 70px;"  value="sign-up">
+  <br>
+</form>
+<label style = "font-size: 12px">already have an account?</label><button style = "font-size: 12px; background-color: transparent; border: 0px; color:#f8312f;  " onclick="logFunction()">log-in!</button>
+</div>
 
-<?php
+  </center>
+</div>
+</div>
+<script>
 
-$query = "SELECT * FROM `contact_list` WHERE `user_id` = '1' LIMIT 10";
+let userId = 0;
+let firstName = "";
+let lastName = "";
 
-$result = mysqli_query($con, $query);
+function signFunction() {
+  var x = document.getElementById("log");
+  var y = document.getElementById("sign");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
+function logFunction() {
+  var x = document.getElementById("sign");
+  var y = document.getElementById("log");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
 
-if (mysqli_num_rows($result) > 0) 
+function doLogin()
 {
-  while($row = mysqli_fetch_assoc($result)) 
+  userId = 0;
+  firstName = "";
+  lastName = "";
+  
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  
+  document.getElementById("loginResult").innerHTML = "";
+
+  let tmp = {username:username,password:password};
+
+  var jsonPayload = JSON.stringify( tmp );
+  
+  let url = 'http://cop4331-8.xyz/API/Login.php';
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  try
+  {
+    xhr.onreadystatechange = function() 
+    {
+      if (this.readyState == 4 && this.status == 200) 
+      {
+        let jsonObject = JSON.parse( xhr.responseText );
+        userId = jsonObject.id;
+    
+        if( userId < 1 )
+        {   
+          document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+          return;
+        }
+
+        saveCookie();
+  
+       window.location.href = "treehouse_host.php";
+      }
+    };
+    xhr.send(jsonPayload);
+  }
+  catch(err)
+  {
+    document.getElementById("loginResult").innerHTML = err.message;
+  }
+}
+
+function doRegister()
+{ 
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  let first_name = document.getElementById("first_name").value;
+  let last_name = document.getElementById("last_name").value;
+  let email = document.getElementById("email").value;
+  let phone_num = document.getElementById("phone_num").value;
+  let address = document.getElementById("address").value;
+  
+  document.getElementById("loginResult").innerHTML = "";
+
+  let tmp = {username:username,password:password,first_name:first_name,last_name:last_name,email:email,phone_num:phone_num,address:address};
+
+  var jsonPayload = JSON.stringify( tmp );
+  
+  let url = 'http://cop4331-8.xyz/API/Register.php';
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  try
+  {
+    xhr.onreadystatechange = function() 
+    {
+      if (this.readyState == 4 && this.status == 200) 
+      {
+        let jsonObject = JSON.parse( xhr.responseText );
+  
+        window.location.href = "index.php";
+      }
+    };
+    xhr.send(jsonPayload);
+  }
+  catch(err)
+  {
+    document.getElementById("loginResult").innerHTML = err.message;
+  }
+}
+
+function saveCookie()
+{
+  let minutes = 20;
+  let date = new Date();
+  date.setTime(date.getTime()+(minutes*60*1000)); 
+  document.cookie = "userId=" + userId + ";expires=" + date.toGMTString(); 
+}
+
+function readCookie()
+{
+  userId = -1;2
+  let data = document.cookie;
+  let splits = data.split(";");
+  for(var i = 0; i < splits.length; i++) 
+  {
+    let thisOne = splits[i].trim();
+    let tokens = thisOne.split("=");
+    
+    if( tokens[0] == "userId" )
+    {
+      userId = parseInt( tokens[1].trim() );
+    }
+  }
+  
+  if( userId < 0 )
+  {
+    window.location.href = "treehouse_host.php";
+  }
+  else
   {
 
-    $username = $row['username'];
-    $password = $row['password'];
-    $firstName = $row['first_name'];
-    $lastName = $row['last_name'];
-    $email = $row['email'];
-    $address = $row['address'];
-  } 
-  } else {
-      echo "<h6 class = 'text-danger'>No data found</h6>";
+    document.getElementById("username").innerHTML = "Logged in as " + firstName + " " + lastName;
+    return userId;
   }
-?>
+}
+
+</script>
+   
+</body>
+</html> 
+
+   
